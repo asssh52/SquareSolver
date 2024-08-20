79a5cc74711b7660a7a068a8c022789021f18ca0
asssh52 <aptemka9000@gmail.com>
2024-08-20 16:01:14 +0300

    Update main.c
    new version

--- main.c
+++ main.c
@@ -1,53 +1,98 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define MAXLINE 1000
-double atof(const char *);
-int sqrtsolver(double a, double b, double c, double s[])
+
+enum solutions{
+    NONE_SOL,
+    ONE_SOL,
+    TWO_SOL,
+    INF_SOL,
+    ERROR
+};
+const double epsilon = 0.000001;
+double input[2] = {0, 0, 0}, roots[1] = {0, 0};
+
+int Check_zero(double a){
+    if (fabs(a) <= epsilon){return 1;}
+    else {return 0;}
+}
+
+int Solve_linear(double b, double c, double *roots)
 {
-    double d, base;
-    if (a == 0){
-        return -1;
-    }
-    d = b * b - 4 * a * c; /* discrim*/
-    base = -1 * b / (2 * a); /* base */
-    if (d < 0){
-        s[2] = -1;
-        return 0;
-    }
-    else if (d == 0){
-        s[0] = base;
-        return 1;
+    if (Check_zero(b) && !Check_zero(c))  {
+        return NONE_SOL;
+    } else if (Check_zero(b) && Check_zero(c))  {
+        return INF_SOL;
+    } else if (!Check_zero(b)) {
+        roots[0] = -c / b;
+        return ONE_SOL;
     } else {
-        s[0] = base + sqrt(d) / (2 * a);
-        s[1] = base - sqrt(d) / (2 * a);
-        s[2] = 0;
-        return 2;
+        return ERROR;
     }
 }
 
-int main() {
-    double a, b, c, s[2];
-    char *kek;
-    size_t maxl = MAXLINE;
-    kek = (char *)malloc(maxl * sizeof(char));
+int Solution_count(double a, double b, double c)
+{
+    double discriminant = b * b - 4 * a * c;
+    if (discriminant > 0){
+        return TWO_SOL;
+    } else if (discriminant == 0){
+        return ONE_SOL;
+    } else {
+        return NONE_SOL;
+    }
+}
+
+int Solve_sqrt(double a, double b, double c, double *roots)
+{
+    if (a == 0){
+        return Solve_linear(b, c, &roots[0]);
+    }
     
-    scanf("%lf%lf%lf", &a, &b, &c);
-    
-    switch (sqrtsolver(a, b, c, s)) {
-        case -1:
-            printf("это не квадратное уравнение\n");
+    switch (Solution_count(a, b, c)) {
+        case TWO_SOL:
+            roots[0] = (-b + sqrt(b * b - 4 * a * c))/(2 * a);
+            roots[1] = (-b - sqrt(b * b - 4 * a * c))/(2 * a);
+            return TWO_SOL;
             break;
-        case 0:
-            printf("нет действительных решений\n");
-            break;
-        case 1:
-            printf("%s%f\n", "одно решение: ",s[0]);
-            break;
-        case 2:
-            printf("%s%f,%f\n", "два решения:",s[0], s[1]);
+        case ONE_SOL:
+            roots[0] = -b / (2 * a);
+            return ONE_SOL;
+        default:
+            return NONE_SOL;
             break;
     }
+}
+
+void Input_coef(void)
+{
+    printf("%s\n", "Введите коэффициенты a,b,c:");
+    if (scanf("%lf%lf%lf", &input[0], &input[1], &input[2]) != 3)
+    {
+        printf("%s\n", "неправильный ввод");
+    }
+}
+
+void Output_function(void)
+{
+    switch (Solve_sqrt(input[0], input[1], input[2], roots)){
+        case NONE_SOL:
+            printf("нет действительных решений\n");
+            break;
+        case ONE_SOL:
+            printf("%s%g\n", "одно решение: ",roots[0]);
+            break;
+        case TWO_SOL:
+            printf("%s%g,%g\n", "два решения:",roots[0], roots[1]);
+            break;
+        case INF_SOL:
+            printf("%s\n", "бесконечно много решений");
+            break;
+    }
+}
+
+int main(void){
+    Input_coef();
+    Output_function();
     return 0;
 }
