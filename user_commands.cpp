12ab8f54c4d5f8891caef7df7a8e3012d570d807
asssh52 <aptemka9000@gmail.com>
2024-08-23 16:14:35 +0300

    +flags

--- user_commands.cpp
+++ user_commands.cpp
@@ -3,31 +3,82 @@
 #include "tester.hpp"
 #include <math.h>
 #include <string.h>
+#include <stdio.h>
 void Input_CommandInput(int argc, char *argv[], equation *keys)
 {
-    double input[3] = {0, 0, 0}, roots[2] = {0, 0};
-    switch(argc){
-        case 1:
-            Start_Default(input, roots);
-            break;
-        case 2:
-            if (strcmp(argv[1], "help") == 0){
-                Start_Help();
-            } else if (strcmp(argv[1], "test") == 0){
-                Do_Tests(0, 9, keys);
-            } else if (strcmp(argv[1], "default") == 0){
-                Start_Default(input, roots);
-            } else {
-                Start_CommandError();
-            }
-            break;
-        default:
-            Start_CommandError();
-            break;
+    flags command_flags = {0, 0, 0};
+    double input[3] = {NAN, NAN, NAN}, roots[2] = {0, 0};
+    if (argc == 1){
+        Start_Default(input, roots);
+    } else {
+        for (int i = 1; i < argc; i++){
+            Check_Flag(argv[i], &command_flags);
+        }
+        Command_Output(keys, input, roots, &command_flags);
     }
 }
+void Command_Output(equation *keys, double *input, double *roots, flags *list_of_flags){
+    if (list_of_flags->list_flag[0]){
+        Start_Help();
+    }
+    if (list_of_flags->list_flag[1]){
+        Do_Tests(0, 9, keys);
+    }
+    if (list_of_flags->list_flag[2]){
+        Start_Default(input, roots);
+    }
+}
+void Check_Flag(char *flag, flags *list_of_flags){
+    if (strcmp(flag, "help") == 0){
+            list_of_flags->list_flag[0] = 1;
+        } else if (strcmp(flag, "test") == 0){
+            list_of_flags->list_flag[1] = 1;
+        } else if (strcmp(flag, "default") == 0){
+            list_of_flags->list_flag[2] = 1;
+        } else {
+            Start_CommandError();
+        }
+}
+/* work in progress
+char *GetConfig_InputFile(){
+    FILE *fp = fopen("config.txt", "r");
+
+    char storage[100][2];
+    char* meow[2] = {&storage[0][0], &storage[0][1]};
+    fscanf(fp, "%s%s", meow);
+    printf("%s\n%s", storage);
+
+    char str[1000];
+    fscanf(fp, "%s", &str[0]);
+    printf("%s", str);
+
+    fclose(fp);
+    return "config.txt";
+}*/
+int Start_Default_File(double *input, double *roots, char *file_link){
+    FILE *fp = fopen(file_link, "r");
+    printf(COLOR_CYAN "\nRead from file: " COLOR_BLUE "%s" COLOR_RESET "\n", file_link);
+
+    if(fp == NULL) {
+        printf("Не удалось открыть файл.\n\n");
+        return 1;
+    }
+    if (fscanf(fp, "%lf%lf%lf", &input[0], &input[1], &input[2]) != 3){
+        printf("Неправильный формат ввода в " COLOR_BLUE "%s" COLOR_RESET ".\n\n", file_link);
+    } else {
+        for (int i = 0; i < 3; i++){
+            if (!isfinite(input[i])){
+                printf("%s\n", "Введите конечные числа.\n\n");
+                return 1;
+            }
+        }
+        Showanswer_Square(input, roots);
+    }
+    fclose(fp);
+    return 0;
+}
 void Start_Default(double *input, double *roots){
-    if (Input_coef(input)){
+    if (!Input_Coef(input)){
         Showanswer_Square(input, roots);
         }
 }
@@ -41,21 +92,20 @@ void Start_CommandError(){
     printf(COLOR_RED "\nError!!!\n" COLOR_RESET "Type " COLOR_CYAN "\'help\'" COLOR_RESET " for help.\n\n");
 }
 
-int Input_coef(double input[])
-{
+int Input_Coef(double input[]){
     printf("\n%s\n", "Введите коэффициенты a,b,c:");
     if (scanf("%lf%lf%lf", &input[0], &input[1], &input[2]) != 3)
     {
         printf("%s\n", "Неправильный ввод");
-        return 0;
+        return 1;
     }
     for (int i = 0; i < 3; i++){
         if (!isfinite(input[i])){
             printf("%s\n", "Введите конечные числа");
-            return 0;
+            return 1;
         }
     }
-    return 1;
+    return 0;
 }
 
 void Showanswer_Square(double input[], double roots[])
@@ -74,28 +124,4 @@ void Showanswer_Square(double input[], double roots[])
             printf("%s\n\n", "Бесконечно много решений");
             break;
     }
-}
-
-void Choose_Path(equation *keys){
-    double input[3] = {0, 0, 0}, roots[2] = {0, 0};
-    switch (Ask_User_Test()){
-    case 1:
-        if (Input_coef(input)){
-        Showanswer_Square(input, roots);
-        }
-        break;
-    case 0:
-        Do_Tests(0, 9, keys);
-        break;
-    default:
-        printf("Неправильный ввод\n");
-        break;
-    }
-}
-
-int Ask_User_Test(){
-    int a = -1;
-    printf("Привет!\nНажми 1 для решения квадратных уравнений или 0 для проверки программы.\n");
-    scanf("%d", &a);
-    return a;
 }
--- user_commands.hpp
+++ user_commands.hpp
@@ -3,14 +3,17 @@
 
 #include <stdio.h>
 #include "tester.hpp"
-
+typedef struct{
+    int list_flag[3];
+} flags;
+int Start_Default_File(double *input, double *roots, char *file_link);
+char *GetConfig_InputFile();
+void Command_Output(equation *keys, double *input, double *roots, flags *list_of_flags);
+void Check_Flag(char *flag, flags *list_of_flags);
 void Input_CommandInput(int argc, char *argv[], equation *keys);
 void Start_Default(double *input, double *roots);
 void Start_Help();
 void Start_CommandError();
 void Showanswer_Square(double input[], double roots[]);
-int Input_coef(double input[]);
-void Choose_Path(equation *keys);
-int Ask_User_Test();
-
+int Input_Coef(double input[]);
 #endif
